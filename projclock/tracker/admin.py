e809d78bf5e330e8442a1c7fc205d4552f6ec094
Allaye <kolade199@gmail.com>
2021-12-21 23:49:47 +0100

    update admin file

--- projclock/tracker/admin.py
+++ projclock/tracker/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Project, ProjectActivity, Employee
+from .models import Project, ProjectActivity
 
 
 # Register your models here.
 
 admin.site.register(Project)
 admin.site.register(ProjectActivity)
-admin.site.register(Employee)
