5c6de92092e12af55921717b58d86fe1a71077a5
Allaye <kolade199@gmail.com>
2021-12-22 13:13:20 +0100

    update and added new endpoints to url path

--- projclock/tracker/urls.py
+++ projclock/tracker/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('create/project', views.CreateProjectApiview.as_view(), name='add_project'),
     path('projects', views.RetriveProjectsApiView.as_view(), name='list_projects'),
+    path('projects/me', views.RetriveMyProjectsApiView.as_view(), name='list_my_projects'),
     path('project/<int:pk>', views.RetrieveUpdateDeleteProjectApiview.as_view(), name='list_update_delete_project'),
+    path('create/activity', views.CreateProjectActivityApiview.as_view(), name='add_activity'),
+    path('activitys', views.RetriveProjectsActivitiesApiView.as_view(), name='list_activities'),
+    path('activity/<int:pk>', views.DestroyProjectActivityApiview.as_view(), name='list_update_delete_activity'),
 ]
