992a07a6b4879060acc263f65526b8b745bb4331
Allaye <kolade199@gmail.com>
2021-12-22 12:33:11 +0100

    created a custom permission class called isowner

---
+++ projclock/employee/permissions.py
@@ -0,0 +1,30 @@
+from rest_framework import permissions
+from employee.models import Employee
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return request.user == Employee.objects.get(id=view.kwargs['id'])
+
+class IsProjectMember(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return request.user == Employee.objects.get(id=view.kwargs['id'])
